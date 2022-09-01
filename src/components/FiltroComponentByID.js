39dc0c58917ee00803a2758e86114f22e1deb6fb
knechtel <maiquelknechtel@gmail.com>
2022-09-01 20:08:01 -0300

    Adicionando margem.

--- src/components/FiltroComponentByID.js
+++ src/components/FiltroComponentByID.js
@@ -3,11 +3,11 @@ import {StyleSheet, TextInput, View, Linking, Text} from 'react-native';
 import CheckBox from '@react-native-community/checkbox';
 import {Button, ScrollView} from 'react-native';
 import {PDF_BY_ID} from '../util/urls';
-const FiltroComponentByID = ({navigation}) => {
+const FiltroComponentByID = ({ navigation }) => {
   const [id, setId] = React.useState();
   const [check, isCheck] = React.useState(false);
   const handleClick = () => {
-    Linking.canOpenURL(PDF_BY_ID + id).then(supported => {
+    Linking.canOpenURL(PDF_BY_ID + id).then((supported) => {
       if (supported) {
         Linking.openURL(PDF_BY_ID + id);
       } else {
@@ -16,12 +16,12 @@ const FiltroComponentByID = ({navigation}) => {
     });
   };
   const callId = () => {
-    navigation.navigate('FormEquipment', {paramKey: id});
+    navigation.navigate("FormEquipment", { paramKey: id });
   };
   const osId = () => {
-    navigation.navigate('FormEquipment', {paramKey: id});
+    navigation.navigate("FormEquipment", { paramKey: id });
   };
-  const select = select => {
+  const select = (select) => {
     if (true) {
       handleClick(id);
     }
@@ -32,19 +32,19 @@ const FiltroComponentByID = ({navigation}) => {
         <TextInput
           style={styles.input}
           value={id}
-          placeholder="ID"
-          onChangeText={id => setId(id)}
+          placeholder="     OS"
+          onChangeText={(id) => setId(id)}
           defaultValue={id}
         />
         <View style={styles1.checkboxContainer}>
           <CheckBox
             style={styles1.checkbox}
             value={check}
-            onValueChange={value => select(value)}
+            onValueChange={(value) => select(value)}
           />
           <Text style={styles1.label}>Gerar PDF!</Text>
         </View>
-        <View style={{marginVertical: 10}}>
+        <View style={{ marginVertical: 10 }}>
           <Button title="Enviar" onPress={() => callId()} />
         </View>
       </ScrollView>
@@ -59,15 +59,19 @@ const styles = StyleSheet.create({
   },
   sectionTitle: {
     fontSize: 24,
-    fontWeight: '600',
+    fontWeight: "600",
   },
   sectionDescription: {
     marginTop: 8,
     fontSize: 18,
-    fontWeight: '400',
+    fontWeight: "400",
   },
   highlight: {
-    fontWeight: '700',
+    fontWeight: "700",
+  },
+  input: {
+    margin: 8,
+    font: 24,
   },
 });
 
