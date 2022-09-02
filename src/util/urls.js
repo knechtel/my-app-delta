2fdf9e2e09e1fae0de7e815089ef50f1b3c79e87
knechtel <maiquelknechtel@gmail.com>
2022-09-02 13:39:59 -0300

    Tornando urls dinamicas.

--- src/util/urls.js
+++ src/util/urls.js
@@ -1,24 +1,23 @@
 export const PADRAO = "http://1da5-177-74-122-85.ngrok.io";
 export const LOCAL = "http://10.0.0.199:5000";
-
-export const CREATE_CLIENT =
-  "http://1da5-177-74-122-85.sa.ngrok.io/api/client-findAll";
-export const FIND_BY_ID_CLIENT = `http://1da5-177-74-122-85.sa.ngrok.io/api/client-findById`;
-export const FIND_ALL_EQUIPMENT_BY_CLIENT = `http://1da5-177-74-122-85.sa.ngrok.io/api/client-findByAllEquipment`;
-export const FIND_EQUIPMENT_BY_CLIENT = `http://1da5-177-74-122-85.sa.ngrok.io/api/equipmentByIdClient`;
-export const FIND_BY_ID_EQUIPMENT = `http://1da5-177-74-122-85.sa.ngrok.io/equipment-findById`;
-export const CREATE_EQUIPMENT = `http://1da5-177-74-122-85.sa.ngrok.io/api/equipment`;
-export const DELETE_CLIENT = `http://1da5-177-74-122-85.sa.ngrok.io/client-delete`;
-export const DELETE_EQUIPMENT = `http://1da5-177-74-122-85.sa.ngrok.io/equipment-delete`;
-export const UPDATE_CLIENT = `http://1da5-177-74-122-85.sa.ngrok.io/api/client-update`;
-export const UPDATE_EQUIPMENT = `http://1da5-177-74-122-85.sa.ngrok.io/api/equipment-update`;
-export const EDIT_CLIENT_URL = "http://localhost:3000/edit/";
-export const EQUIPMENT_FIND_DATA_ENTRADA = `http://1da5-177-74-122-85.sa.ngrok.io/api/equipment-findByDataEntrada`;
-export const FIND_ALL_CLIENT = `http://1da5-177-74-122-85.sa.ngrok.io/api/client-findAll`;
-export const PDF_BY_ID = "http://1da5-177-74-122-85.sa.ngrok.io/download?id=";
-export const LOAD_IAMAGE = "http://1da5-177-74-122-85.ngrok.sa.io/img/";
-export const UPLOAD_IAMAGE = "http://1da5-177-74-122-85.sa.ngrok.io/upOs?id=";
-export const EQUIPMENT_FIND_DATA_ENTREGA =
-  "http://1da5-177-74-122-85.sa.ngrok.io/api/equipment-findByDataEntrega";
-export const PHOTO_SERVIDOR =
-  "http://1da5-177-74-122-85.ngrok.io/print/download";
+var BASE = "http://c509-177-74-122-85.sa.ngrok.io";
+export var CREATE_CLIENT = BASE + "/api/client-findAll";
+export var FIND_BY_ID_CLIENT = BASE + `/api/client-findById`;
+export var FIND_ALL_EQUIPMENT_BY_CLIENT = `http://1da5-177-74-122-85.sa.ngrok.io/api/client-findByAllEquipment`;
+export var FIND_EQUIPMENT_BY_CLIENT = BASE + `/api/equipmentByIdClient`;
+export var FIND_BY_ID_EQUIPMENT = BASE + `/equipment-findById`;
+export var CREATE_EQUIPMENT = `http://1da5-177-74-122-85.sa.ngrok.io/api/equipment`;
+export var DELETE_CLIENT = `http://1da5-177-74-122-85.sa.ngrok.io/client-delete`;
+export var DELETE_EQUIPMENT = `http://1da5-177-74-122-85.sa.ngrok.io/equipment-delete`;
+export var UPDATE_CLIENT = `http://1da5-177-74-122-85.sa.ngrok.io/api/client-update`;
+export var UPDATE_EQUIPMENT = BASE + `/api/equipment-update`;
+export var EDIT_CLIENT_URL = "http://localhost:3000/edit/";
+export var EQUIPMENT_FIND_DATA_ENTRADA =
+  BASE + `/api/equipment-findByDataEntrada`;
+export var FIND_ALL_CLIENT = BASE + `/api/client-findAll`;
+export var PDF_BY_ID = BASE + "/download?id=";
+export var LOAD_IAMAGE = BASE + "/img/";
+export var UPLOAD_IAMAGE = BASE + "/upOs?id=";
+export var EQUIPMENT_FIND_DATA_ENTREGA =
+  BASE + "/api/equipment-findByDataEntrega";
+export var PHOTO_SERVIDOR = BASE + "/print/download";
