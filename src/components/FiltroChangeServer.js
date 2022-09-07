3fb212e417d8019994ec1431e089f8ed969082a4
knechtel <maiquelknechtel@gmail.com>
2022-09-07 10:38:11 -0300

    Adicionando possibilidade de troca de servidor.

---
+++ src/components/FiltroChangeServer.js
@@ -0,0 +1,70 @@
+import React from "react";
+import { StyleSheet, TextInput, View } from "react-native";
+import BASE from "../util/urls";
+import { Button, ScrollView } from "react-native";
+
+const FiltroChangeServer = ({ navigation }) => {
+  const [name, setName] = React.useState();
+  const [check, isCheck] = React.useState(false);
+
+  const callId = (name) => {
+    BASE = name;
+  };
+
+  return (
+    <>
+      <ScrollView keyboardShouldPersistTaps="always">
+        <TextInput
+          style={styles.input}
+          value={name}
+          placeholder="Novo Endereço"
+          onChangeText={(name) => setName(name)}
+          defaultValue={name}
+        />
+
+        <View style={{ marginVertical: 10 }}>
+          <Button title="Enviar" onPress={() => callId(name)} />
+        </View>
+      </ScrollView>
+    </>
+  );
+};
+
+const styles = StyleSheet.create({
+  sectionContainer: {
+    marginTop: 32,
+    paddingHorizontal: 24,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: "600",
+  },
+  sectionDescription: {
+    marginTop: 8,
+    fontSize: 18,
+    fontWeight: "400",
+  },
+  highlight: {
+    fontWeight: "700",
+  },
+});
+
+const styles1 = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  checkboxContainer: {
+    flexDirection: "row",
+    marginBottom: 20,
+  },
+  checkbox: {
+    alignSelf: "center",
+  },
+  label: {
+    margin: 8,
+  },
+});
+
+export default FiltroChangeServer;
