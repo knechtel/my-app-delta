4033b2f61204f6fd5d0e08d480819430043d1e2a
knechtel <maiquelknechtel@gmail.com>
2022-12-11 16:17:57 -0300

    Adicionando lógica de login.

--- src/actions/loginApi.js
+++ src/actions/loginApi.js
@@ -1,23 +1,64 @@
 import axios from "axios";
-import { LOGIN } from "../util/urls";
+import { LOGIN, REFRESH } from "../util/urls";
 
-async function login(email, password) {
+const login = async (email, password) => {
   var token;
-  await axios({
-    method: "post",
-    url: LOGIN,
-    headers: {
-      "Content-type": "application/json",
-    },
-    data: {
-      email: email,
-      password: password,
-    },
-  }).then((response) => {
-    //console.log(response.data);
-    token = response.data;
-  });
+  // fetch(LOGIN, {
+  //   method: "POST",
+  //   headers: {
+  //     Accept: "application/json",
+  //     "Content-Type": "application/json",
+  //   },
+  //   body: JSON.stringify({
+  //     email: email,
+  //     password: password,
+  //   }),
+  // }).then((response) => {
+  //   token = response.data;
+  //   console.log(token)
+  // });
+
+  try {
+    console.log("Inside function login");
+    await axios({
+      method: "POST",
+      url: LOGIN,
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE",
+        "Content-Type": "application/json",
+        Pragma: "no-cache",
+      },
+      data: JSON.stringify({ email: email, password: password }),
+    }).then((response) => {
+      console.log("mais um teste! ");
+      console.log(response.data);
+      token = response.data;
+    });
+  } catch (err) {
+    console.log(err);
+    if (err.response.status == 405) {
+      console.log("entrou aqui ...");
+      await axios({
+        method: "GET",
+        url: REFRESH,
+        headers: {
+          "Access-Control-Allow-Origin": "*",
+          "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE",
+          "Content-Type": "application/json",
+          Pragma: "no-cache",
+        },
+        data: JSON.stringify({ email: email, password: password }),
+      }).then((response) => {
+        console.log("mais um teste! ");
+        console.log(response.data);
+        token = response.data;
+      });
+    }
+    throw new Error("Unable to get a token.");
+  }
+
   return token["access_token"];
-}
+};
 
 export { login };
